28cbf8b3789c8c7d6a211aae325bc598e418deff
HolyBoley4 <holyboley4@icloud.com>
2024-11-20 11:28:18 -0800

    ETL Scripts

---
+++ FinalUserInterface/ETL/IXLCell.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ExcelToSQL
+{
+    internal class PoopCell
+    {
+        public object Address { get; internal set; }
+        public object Value { get; internal set; }
+
+        internal ReadOnlySpan<byte> GetValue<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool TryGetValue<T>(out T dateValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
