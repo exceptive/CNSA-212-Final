65375cd68e35d5e3b187d88c578e879ff4b516ff
HolyBoley4 <holyboley4@icloud.com>
2024-11-20 11:25:49 -0800

    Delete FinalUserInterface/XLWorkbook.cs

--- FinalUserInterface/XLWorkbook.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExcelToSQL
-{
-    // Renamed the class from XLWorkbook to CustomXLWorkbook
-    class CustomXLWorkbook  // New name to avoid conflict
-    {
-        // Your custom code
-    }
-}
